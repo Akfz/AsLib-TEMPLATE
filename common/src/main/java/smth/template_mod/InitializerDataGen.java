c8f7d1f08e1e1a10d23b5799b6047c2caf55af4e
akaize <sahsa61842@gmail.com>
2026-08-07 04:39:07 +0300

    upd

---
+++ common/src/main/java/smth/template_mod/InitializerDataGen.java
@@ -0,0 +1,22 @@
+/*
+package smth.template_mod;
+
+import v.akfz.aslib.annotation.DontCompile;
+import v.akfz.aslib.datagen.fabric.mod.FabricModJsonData;
+import v.akfz.aslib.datagen.fabric.mod.GenerateFabricModJson;
+import v.akfz.aslib.datagen.forge.modstoml.GenerateModsToml;
+import v.akfz.aslib.datagen.forge.modstoml.ModsTomlData;
+import v.akfz.aslib.datagen.forge.packmcmeta.GeneratePackMcmeta;
+import v.akfz.aslib.datagen.forge.packmcmeta.PackMcmetaData;
+
+@DontCompile
+public class InitializerDataGen {
+    //генерирует все что нужно для запуска мода
+    public static void main(String[] args) {
+        //с entrypoint, там просто нужно писать путь до класс и добавлять fabric-prefix(дефолт _fabric), его можно в аннотации менять
+        new GenerateFabricModJson(new FabricModJsonData().mixin("template.mixins.json").entrypoint("v.akfz.aslib.AsLib_fabric")).run("common");
+        new GenerateModsToml(new ModsTomlData()).run("common");
+        new GeneratePackMcmeta(new PackMcmetaData()).run("common");
+    }
+}
+ */
